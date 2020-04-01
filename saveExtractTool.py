b628c1125b1f6df28351a40a2d60781895bfe035
JasonCubed132 <amcb2002@googlemail.com>
2020-04-01 21:20:07 +0100

    start replacing ninfs code with pyctr

--- saveExtractTool.py
+++ saveExtractTool.py
@@ -1,36 +1,36 @@
 import sys, os, pathlib
 import os.path
 import xml.etree.ElementTree as ET
-f
+
+try:
+    import pyctr
+    from pyctr.crypto import CryptoEngine, Keyslot
+    from pyctr.type.ncch import NCCHReader
+    from pyctr.type.tmd import TitleMetadataReader
+    
+except ImportError:
+    print("Error pyctr version 0.1.0 is required")
+    exit()
 
 def main():
     print(len(sys.argv))
-    if len(sys.argv) < 15:
+    if len(sys.argv) < 9:
         print("""Arguments missing. Required arguments are:
 Nintendo 3DS folder
 --inputFolder folderPath
-Free drive letter:
---drive driveLetter
 Movable.sed path:
 --movable movablePath
 Output folder:
 --outputFolder folderPath
-Path to ninfs.exe:
---ninfs ninfsPath
 Path to disa-extract.py:
 --disa disaPath
-Path to 3dsreleases.xml:
---releases 3dsReleasesPath
 """)
         return None
 
     inputFolder = None
     outputFolder = None
     movablePath = None
-    driveLetter = None
-    ninfsPath = None
     disaPath = None
-    releasesPath = None
     
     i = 1
     while i < len(sys.argv):
@@ -44,37 +44,24 @@ Path to 3dsreleases.xml:
         elif sys.argv[i] == "--movable" or sys.argv[i] ==  "-m":
             i += 1
             movablePath = sys.argv[i]
-        elif sys.argv[i] == "--drive" or sys.argv[i] ==  "-d":
-            i += 1
-            driveLetter = sys.argv[i]
-        elif sys.argv[i] == "--ninfs" or sys.argv[i] ==  "-n":
-            i += 1
-            ninfsPath = sys.argv[i]
         elif sys.argv[i] == "--disa" or sys.argv[i] ==  "-e":
             i += 1
             disaPath = sys.argv[i]
-        elif sys.argv[i] == "--releases" or sys.argv[i] ==  "-r":
-            i += 1
             releasesPath = sys.argv[i]
         else:
             print("Unable to parse {}".format(sys.argv[i]))
         i += 1
+        
     if inputFolder is None:
         print("Input folder is not present")
     if outputFolder is None:
         print("Output folder is not present")
     if movablePath is None:
         print("Movable path is not present")
-    if driveLetter is None:
-        print("Drive letter is not present")
-    if ninfsPath is None:
-        print("Path to ninfs is not present")
     if disaPath is None:
         print("Path to disa-extract.py is not present")
-    if releasesPath is None:
-        print("Path to 3dsreleases.py is not present")
     
-    if inputFolder is None or outputFolder is None or movablePath is None or driveLetter is None or ninfsPath is None or disaPath is None or releasesPath is None:
+    if inputFolder is None or outputFolder is None or movablePath is None or disaPath is None :
         return None
 
     print("All paths parsed")
@@ -84,30 +71,69 @@ Path to 3dsreleases.xml:
         print("Output folder directory does not exist. Inputted path: {}".format(outputFolder))
     if not(os.path.isfile(movablePath)):
         print("movable does not exist. Inputted path: {}".format(movablePath))
-    if not(os.path.isfile(ninfsPath)):
-        print("ninfs.exe does not exist. Inputted path: {}".format(ninfsPath))
     if not(os.path.isfile(disaPath)):
         print("disa_extract.py does not exist. Inputted path: {}".format(disaPath))
-    if not(os.path.isfile(releasesPath)):
-        print("3dsreleases.xml does not exist. Inputted path: {}".format(releasesPath))
 
-    print("Attempting to mount {} to {} using {} by using {}".format(inputFolder, driveLetter, movablePath, ninfsPath))
-    command = "\"" + ninfsPath + "\" sd -f \"" + inputFolder + "\" " + driveLetter + ": --movable \"" + movablePath + "\" -r"
-    print(command)
-    stream = os.popen(command)
-    output = stream.read()
-    print(output)
+    print("Creating crypto object, if this creates an error then boot9.bin is not found")
+    crypto = CryptoEngine()
 
-    if not(pathlib.Path(driveLetter + ":").is_dir()):
-        print("Failed to mount")
-        return None
+    print("Setting movable.sed for decryption")
+    crypto.setup_sd_key_from_file(movablePath)
 
-    directories = os.listdir(driveLetter + ":")
+    print("Getting directories in Nintendo 3DS")
+    #id0 = selectDirectory32char(inputFolder)
+    id0 = crypto.id0
+    id0 = id0.hex()
+    
+    print("Getting directories in {}".format(id0))
+    id1 = selectDirectory32char(inputFolder + "\\" + id0)
+    
+    print("Starting dump and decrypt.")
 
+    for titleHigh in os.listdir(inputFolder + "\\" + id0 + "\\" + id1 + "\\title\\"):
+        print("Title High: {}".format(titleHigh))
+        for titleLow in os.listdir(inputFolder + "\\" + id0 + "\\" + id1 + "\\title\\" + titleHigh + "\\"):
+            print("Title Low: {}".format(titleLow))
+            appFile = selectAppFile(crypto, inputFolder + "\\" + id0 + "\\" + id1 + "\\title\\" + titleHigh + "\\" + titleLow + "\\" + "content" + "\\")
+            return None
+
+def selectAppFile(crypto, inputPath):
+    files = os.listdir(inputPath)
+
+    tmdFile = None
+    
+    for i in range(len(files)):
+        if ".tmd" in files[i] and tmdFile == None:
+            tmdFile = files[i]
+
+    path = inputPath + tmdFile
+
+    split_path = path.split("\\")
+    sd_path = "/" + split_path[len(split_path)-5]
+    for i in range(len(split_path)-4, len(split_path)):
+        sd_path += "/" + split_path[i]
+    
+    print(path)
+    print(sd_path)
+    with open(path) as fh:
+        with crypto.create_ctr_io(Keyslot.SD, fh, crypto.sd_path_to_iv(sd_path)) as sd_file:
+            tmd = TitleMetadataReader.load(sd_file)
+            first_record = tmd.chunk_records[0]
+            first_record_id = first_record.id
+
+    print(first_record_id)
+    return None
+def selectDirectory32char(inputPath):
+    directories = os.listdir(inputPath)
+    
+    for i in range(len(directories)):
+        if len(directories[i]) != 32:
+            directories.pop(i)
+            
     if len(directories) == 1:
-        directory == directories[0]
-    elif len(directory) == 0:
-        print("No folders in mounted directores.")
+        directory = directories[0]
+    elif len(directories) == 0:
+        print("No folders in {}".format(inputPath))
         return None
     else:
         directory = None
@@ -123,19 +149,7 @@ Path to 3dsreleases.xml:
                 print("Inputted value is not valid.")
             else:
                 directory = directories[directoryNum]
-    
-    print("Opening 3dsreleases.xml")
-    try:
-        3dsreleases = open(releasesPath, "r")
-    except FileNotFoundError:
-        print("File not found, this should have been detected earlier.")
-    
-    print("Starting dump and decrypt.")
+    return directory
 
-    for titleHigh in os.listdir(driveLetter + ":\\" + directory + "\\title\\"):
-        print("Title High: {}".format(titleHigh))
-        for titleLow in os.listdir(driveLetter + ":\\" + directory + "\\title\\" + titleHigh + "\\"):
-            print("Title Low: {}".format(titleLow))
-            
 if __name__ == "__main__":
     main()
